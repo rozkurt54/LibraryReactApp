b05596c32e20197b6210728d2605e8fad614b340
Ramazan ÖZKURT <ramazanozkurt@gmail.com>
2022-11-24 00:19:43 +0300

    4. Ders

--- src/Component/Author.js
+++ src/Component/Author.js
@@ -4,7 +4,13 @@ const Author = ({ author }) => {
     return (
         <div className="border border-slate-200 m-2 rounded p-1 shadow-lg">
             <div className="text-purple-800 font-bold">{author.name}</div>
-            <div className="text-purple-800 font-bold">{author.id}</div>
+            <div className="text-purple-800 font-bold">
+                {author.id}
+                <img
+                    src="http://localhost:8081/images/7a9472a7-afce-4e57-bfb6-914488407315.jpg"
+                    alt={author.name}
+                />
+            </div>
         </div>
     );
 };
